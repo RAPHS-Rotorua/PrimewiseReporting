c74f8f0592461878364495e4d531723eae956954
JustinSherborneRaphs <justin.sherborne@raphs.org.nz>
2025-03-03 13:11:29 +1300

    changed the rpt_IMMS_24M_Q4_Summary.sql to reference the new .72 performance target

--- PrimewiseReporting/dbo/Stored Procedures/rpt_IMMS_24M_Q4_Summary.sql
+++ PrimewiseReporting/dbo/Stored Procedures/rpt_IMMS_24M_Q4_Summary.sql
@@ -1,62 +1,78 @@
-﻿CREATE Procedure [dbo].[rpt_IMMS_24M_Q4_Summary] @PracticeIDs VARCHAR(MAX)
-as
+﻿CREATE PROCEDURE [dbo].[rpt_IMMS_24M_Q4_Summary]
+	  @PracticeIDs VARCHAR(MAX)
+AS
 
-/*
-AUTHOR:						Justin Sherborne
-CREATE DATE:				17 Sep 2024											
- 		
-*/
+	/*
+	AUTHOR:						Justin Sherborne
+	CREATE DATE:				17 Sep 2024											
+	 		
+	*/
 
-DECLARE @AuthorisedPracticeIDList TABLE
-		(
-		PracticeID INT
-		)		
-	INSERT INTO	@AuthorisedPracticeIDList(PracticeID)
+	DECLARE @AuthorisedPracticeIDList TABLE (
+		  PracticeID INT
+	)
+	INSERT INTO @AuthorisedPracticeIDList (PracticeID)
 	EXEC [dbo].[Report_GetAuthorisedPracticeIDs];
 
-	
-	--- Get the selected ID's
-	DECLARE @SelectedPracticeIDList TABLE
-		(
-		PracticeID INT
-		)		
-	INSERT INTO	@SelectedPracticeIDList(PracticeID)
-	SELECT NUMBER FROM PrimeWiseReporting.[dbo].[fn_SplitInt] (@PracticeIDs, ',');
 
-With RAPHS_Cohort
-as
-(
-Select Cohort, Ethnicity,Sum(Denominator) as 'Denominator', Sum(Numerator) as Completed,
-cast(Sum(Numerator) as decimal(10,2))/cast(Sum(Denominator) as decimal(10,2)) as performance,
-0.682 as Target, ceiling((0.682 * cast(Sum(Denominator) as decimal(10,2))) - cast(Sum(Numerator) as decimal(10,2))) as Gap, 1 as orderby
-from
-(Select 
-distinct
-PracticeID, 'RAPHS Network' as Cohort, NHI,iif(Ethnicity = 'NZ Maori', 'Maori','Non-Maori') as 'Ethnicity',
-1 as Denominator, Numerator_24_Months_Qtr4 as Numerator
-from IMMS_Childhood_Detail  --Select * from IMMS_Childhood_Detail
-where SLM_Age_24_Months_Qtr4 = 1) as z
-group by Cohort, Ethnicity
-)
-,
-Practice_Cohort
-as
-(
-Select Cohort, Ethnicity,Sum(Denominator) as 'Denominator', Sum(Numerator) as Completed,
-cast(Sum(Numerator) as decimal(10,2))/cast(Sum(Denominator) as decimal(10,2)) as performance,
-0.682 as Target, ceiling((0.682 * cast(Sum(Denominator) as decimal(10,2))) - cast(Sum(Numerator) as decimal(10,2))) as Gap, 2 as orderby
-from
-(
-Select 
-distinct
-Practice as Cohort,NHI, iif(Ethnicity = 'NZ Maori', 'Maori','Non-Maori') as Ethnicity,
-1 as Denominator, Numerator_24_Months_Qtr4 as Numerator
-from IMMS_Childhood_Detail as i
-JOIN @SelectedPracticeIDList as p on p.PracticeID = i.PracticeID
-where SLM_Age_24_Months_Qtr4 = 1
-) as z
-Group by Cohort, Ethnicity
-)
-Select * from RAPHS_Cohort
-union all
-Select * from Practice_Cohort;
+	--- Get the selected ID's
+	DECLARE @SelectedPracticeIDList TABLE (
+		  PracticeID INT
+	)
+	INSERT INTO @SelectedPracticeIDList (PracticeID)
+	SELECT NUMBER
+	FROM PrimeWiseReporting.[dbo].[fn_SplitInt](@PracticeIDs, ',');
+
+	WITH RAPHS_Cohort AS
+	(
+		SELECT Cohort
+			 , Ethnicity
+			 , SUM(Denominator) AS 'Denominator'
+			 , SUM(Numerator) AS Completed
+			 , CAST(SUM(Numerator) AS DECIMAL(10, 2)) / CAST(SUM(Denominator) AS DECIMAL(10, 2)) AS performance
+			 , 0.72 AS Target  --on 2025-3-3: JS altered to reflect new target (was 0.682 prior to this)
+			 , CEILING((0.72 * CAST(SUM(Denominator) AS DECIMAL(10, 2))) - CAST(SUM(Numerator) AS DECIMAL(10, 2))) AS Gap --on 2025-3-3: JS altered to reflect new target (was 0.682 prior to this)
+			 , 1 AS orderby
+		FROM (
+			SELECT
+			DISTINCT PracticeID
+				   , 'RAPHS Network' AS Cohort
+				   , NHI
+				   , IIF(Ethnicity = 'NZ Maori', 'Maori', 'Non-Maori') AS 'Ethnicity'
+				   , 1 AS Denominator
+				   , Numerator_24_Months_Qtr4 AS Numerator
+			FROM IMMS_Childhood_Detail  --Select * from IMMS_Childhood_Detail
+			WHERE SLM_Age_24_Months_Qtr4 = 1
+		) AS z
+		GROUP BY Cohort
+			   , Ethnicity
+	),
+	Practice_Cohort AS
+	(
+		SELECT Cohort
+			 , Ethnicity
+			 , SUM(Denominator) AS 'Denominator'
+			 , SUM(Numerator) AS Completed
+			 , CAST(SUM(Numerator) AS DECIMAL(10, 2)) / CAST(SUM(Denominator) AS DECIMAL(10, 2)) AS performance
+			 , 0.72 AS Target --on 2025-3-3: JS altered to reflect new target (was 0.682 prior to this)
+			 , CEILING((0.72 * CAST(SUM(Denominator) AS DECIMAL(10, 2))) - CAST(SUM(Numerator) AS DECIMAL(10, 2))) AS Gap --on 2025-3-3: JS altered to reflect new target (was 0.682 prior to this)
+			 , 2 AS orderby
+		FROM (
+			SELECT
+			DISTINCT Practice AS Cohort
+				   , NHI
+				   , IIF(Ethnicity = 'NZ Maori', 'Maori', 'Non-Maori') AS Ethnicity
+				   , 1 AS Denominator
+				   , Numerator_24_Months_Qtr4 AS Numerator
+			FROM IMMS_Childhood_Detail AS i
+				JOIN @SelectedPracticeIDList AS p ON p.PracticeID = i.PracticeID
+			WHERE SLM_Age_24_Months_Qtr4 = 1
+		) AS z
+		GROUP BY Cohort
+			   , Ethnicity
+	)
+	SELECT *
+	FROM RAPHS_Cohort
+	UNION ALL
+	SELECT *
+	FROM Practice_Cohort;
